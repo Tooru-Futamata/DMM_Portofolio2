a318b67d36557cd65ce87da20eece4648c8520e3
Sayo-Shiozuka <sayosf1026@gmail.com>
2021-04-07 07:41:16 +0000

    [add]user/show/edit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -4,18 +4,37 @@ class Public::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to public_user_path(current_user.id)
+    end
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to public_user_path(@user.id), notice: 'You have updated user successfully'
+    else
+      render :edit
+    end
   end
 
   def followings
   end
 
   def followers
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :address, :profile_image, :email)
   end
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1d01cad994ff7d992c6bd74b6b110beaaeb9e2c078ba9394d372b87491af01a9a26de10021406a2d351ba262bc5bff14958a68fb64e411da7a98bfd443c8addf'
