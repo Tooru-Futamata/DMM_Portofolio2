36d4eb9ac2dbe79598617357507c07ba5b0ffe7a
Sayo-Shiozuka <sayosf1026@gmail.com>
2021-04-10 13:22:34 +0000

    [add]gem:ransack

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,8 @@ class Post < ApplicationRecord
 
   attachment :image
 
+  validates :title, presence: true
+
   validates :rate, numericality: {
     less_than_or_equal_to: 5,
     greater_than_or_equal_to: 1}, presence: true
