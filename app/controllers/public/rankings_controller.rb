d8a703fc8725d38ab0e0eb82235a80d59e7dc9e8
Sayo-Shiozuka <sayosf1026@gmail.com>
2021-04-11 14:55:46 +0000

    [add]ranking

--- app/controllers/public/rankings_controller.rb
+++ app/controllers/public/rankings_controller.rb
@@ -1,6 +1,12 @@
 class Public::RankingsController < ApplicationController
 
   def index
+    @golf_courses = GolfCourse.
+              find(Post.
+                    group(:rate).
+                    order('avg(rate) desc').
+                    pluck(:golf_course_id)
+                  )
   end
 
   def show
