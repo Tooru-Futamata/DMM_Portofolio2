4010d931a228fd19c36862c06da7773a24a9e02e
Sayo-Shiozuka <sayosf1026@gmail.com>
2021-04-13 06:36:42 +0000

    [add]topページ

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,11 @@
 class HomesController < ApplicationController
   def top
-  end
-
-  def about
+    @golf_courses = GolfCourse.
+              find(Post.
+                    limit(4).
+                    group(:rate).
+                    order('avg(rate) desc').
+                    pluck(:golf_course_id)
+                  )
   end
 end
