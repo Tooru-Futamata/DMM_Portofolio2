812840fef0ab665a3023cfc1aa022ac21a182ed9
Sayo-Shiozuka <sayosf1026@gmail.com>
2021-04-19 11:54:21 +0000

    [add]GoogleMapAPI実装完了

--- app/controllers/admin/golf_courses_controller.rb
+++ app/controllers/admin/golf_courses_controller.rb
@@ -48,7 +48,7 @@ class Admin::GolfCoursesController < ApplicationController
   private
 
   def golf_course_params
-    params.require(:golf_course).permit(:name,:image, :region_id, :prefecture, :address )
+    params.require(:golf_course).permit(:name,:image, :region_id, :prefecture, :address, :latitude, :longitude )
   end
 
 end
