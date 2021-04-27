02a1b8b57a9a28ae92aa7eb1a038c2a0c8926227
Sayo-Shiozuka <sayosf1026@gmail.com>
2021-04-27 15:38:47 +0000

    profile_imageからmypageへの遷移エラー解消

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,17 +1,26 @@
 class Public::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :destroy, :followings, :followers]
+  before_action :require_user_logged_in, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  # before_action :admin_user, only: :destroy
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
-    @posts = @user.posts.all.page(params[:page]).per(6)
+    # @user = User.find(params[:id])
+    # @posts = @user.posts.all.page(params[:page]).per(6)
+    @posts = Post.includes(:user, :golf_course).where(user_id: @user.id).page(params[:page]).per(6)
+    # counts(@user)
     likes = Like.where(user_id: @user.id).pluck(:golf_course_id) # ログイン中のユーザーのお気に入りのgolfcourse_idカラムを取得
     @like_list = GolfCourse.find(likes) # gole_coursesテーブルから、お気に入り登録済みのレコードを取得
+
+
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     if @user == current_user
       render "edit"
     else
@@ -20,7 +29,7 @@ class Public::UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to public_user_path(@user.id), notice: 'You have updated user successfully'
     else
@@ -29,12 +38,12 @@ class Public::UsersController < ApplicationController
   end
 
   def followings
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     @users = @user.followings.all
   end
 
   def followers
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     @users = @user.followers.all
   end
 
@@ -42,5 +51,14 @@ class Public::UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image, :email)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(current_user) unless @user == current_user
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
