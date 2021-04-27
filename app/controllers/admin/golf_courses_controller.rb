37f34bc86b19f3b6ff162fbfd454f65bffc881df
Sayo-Shiozuka <sayosf1026@gmail.com>
2021-04-27 18:01:42 +0000

    [add]seed:golfcourse

--- app/controllers/admin/golf_courses_controller.rb
+++ app/controllers/admin/golf_courses_controller.rb
@@ -6,7 +6,15 @@ class Admin::GolfCoursesController < ApplicationController
   end
 
   def index
-    @golf_courses = params[:region_id].present? ? GolfCourse.where(region_id: params[:region_id]).page(params[:page]).per(8) : GolfCourse.all.page(params[:page]).per(8)
+    # @golf_courses = params[:region_id].present? ? GolfCourse.where(region_id: params[:region_id]).page(params[:page]).per(8) : GolfCourse.all.page(params[:page]).per(8)
+    if params[:region_id].present? # region_idで絞った時
+      @golf_courses = GolfCourse.where(region_id: params[:region_id]).page(params[:page]).per(8)
+    elsif params[:prefecture_id].present? # 県名で絞った時
+      @golf_courses = GolfCourse.where(prefecture_id: params[:prefecture_id]).page(params[:page]).per(8)
+    else # それ以外の処理（ヘッダーからランキングページに遷移した時）
+      @golf_courses = GolfCourse.all.page(params[:page]).per(8)
+    end
+
     @regions = Region.all
     @prefectures = Prefecture.all
   end
