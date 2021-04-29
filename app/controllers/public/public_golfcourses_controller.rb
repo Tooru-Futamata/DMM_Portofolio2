6aeb752aa5d80509e9743a330f8f5cd89f228987
Sayo-Shiozuka <sayosf1026@gmail.com>
2021-04-29 13:12:58 +0000

    [delete]comment_controller,[add]コメントアウト

--- app/controllers/public/public_golfcourses_controller.rb
+++ app/controllers/public/public_golfcourses_controller.rb
@@ -1,6 +1,8 @@
 class Public::PublicGolfcoursesController < ApplicationController
   def index
+    # 三項演算子 → <条件式> ? true の時の処理 : false の時の処理
     # @golf_courses = params[:region_id].present? ? GolfCourse.where(region_id: params[:region_id]).page(params[:page]).per(8) : GolfCourse.all.page(params[:page]).per(8)
+
     if params[:region_id].present? # region_idで絞った時
       @golf_courses = GolfCourse.where(region_id: params[:region_id]).page(params[:page]).per(8)
     elsif params[:prefecture_id].present? # 県名で絞った時
@@ -9,8 +11,8 @@ class Public::PublicGolfcoursesController < ApplicationController
       @golf_courses = GolfCourse.all.page(params[:page]).per(8)
     end
 
-    @regions = Region.all
-    @prefectures = Prefecture.all
+    @regions = Region.all #地方名で絞るボタン
+    @prefectures = Prefecture.all　#県名で絞るボタン
   end
 
   def show
